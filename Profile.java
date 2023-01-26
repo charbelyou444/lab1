95a4ae6c2e4750d742832b1669baaa0ee93c4ff3
charbel youssef <charbelyoussef444@gmail.com>
2023-01-26 20:32:40 +0200

    initial info provided

---
+++ Profile.java
@@ -0,0 +1,8 @@
+public class Profile 
+{	
+	public static void main(String[] args){
+		System.out.print("charbelyoussef");
+		System.out.println("mechatronics");
+		System.out.println("1styear");
+	}
+}
